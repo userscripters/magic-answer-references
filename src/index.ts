8e7baf314581221a0aadf47db8b5bd147c83eb50
Oleg Valter <o.a.philippov@gmail.com>
2022-05-05 17:51:07 +0300

    added helper for making Stacks buttons

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ type StacksTextInputOptions = {
     value?: string;
 };
 
+type StacksButtonOptions = {
+    classes?: string[];
+    title?: string;
+    muted?: boolean;
+    type?: "outlined" | "filled";
+    primary?: boolean;
+    danger?: boolean;
+    loading?: boolean;
+};
+
 type StacksTableOptions = {
     cellGrid?: Array<string | Node>[];
     headers: Array<string | Node>;
@@ -344,6 +354,46 @@ window.addEventListener("load", async () => {
         ];
 
         rules.forEach((rule) => sheet.insertRule(rule));
+    };
+
+    /**
+     * @see https://stackoverflow.design/product/components/buttons/
+     *
+     * @summary creates a stacks button
+     * @param id id of the button
+     * @param text text of the button
+     * @param options configuration
+     */
+    const makeStacksButton = (
+        id: string,
+        text: string,
+        {
+            classes = [],
+            title,
+            danger = false,
+            loading = false,
+            muted = false,
+            primary = false,
+            type = "filled",
+        }: StacksButtonOptions = {}
+    ) => {
+        const btn = document.createElement("button");
+        btn.id = id;
+        btn.textContent = text;
+        btn.classList.add("s-btn", `s-btn__${type}`, ...classes);
+        btn.setAttribute("role", "button");
+        btn.setAttribute("aria-label", title || text);
+
+        if (danger) btn.classList.add("s-btn__danger");
+        if (muted) btn.classList.add("s-btn__muted");
+        if (primary) btn.classList.add("s-btn__primary");
+        if (loading) btn.classList.add("is-loading");
+
+        if (title) {
+            btn.title = title;
+        }
+
+        return btn;
     };
 
     /**
