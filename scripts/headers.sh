fa357c47df5c5e55dcdc3c990af191c05a8732fc
Oleg Valter <o.a.philippov@gmail.com>
2022-05-05 21:28:56 +0300

    added @userscripters/storage to @require headers

--- scripts/headers.sh
+++ scripts/headers.sh
@@ -6,6 +6,8 @@ output=$dist"/headers.js"
 generate tampermonkey \
     -o $output \
     -m all meta "https://domain/questions/*" \
+    --require "https://github.com/userscripters/storage/raw/master/dist/browser.js" \
+    --grant get set delete \
     --pretty
 
 userscript="$(find -iwholename "./$dist/*\.js" -type f -not -iname "*headers\.js")"
